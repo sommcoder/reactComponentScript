aafe1be6ecd45961f3412d640ffc4d14a7acf0a1
Brian Davies <brian.davies589@gmail.com>
2022-11-03 10:14:06 -0400

    set UTF encoding to UTF-8 which should aviod the hidden character issue.

--- reactComponentScript.ps1
+++ reactComponentScript.ps1
@@ -1,36 +1,38 @@
+# cd to the directory that contains this file and is where you want to add the new folder/js file
 # command: PowerShell -File newReactComponent.ps1
+# asks user for the name of the component
+# asks user for the HTML element to be used as the styled-component
 
 # assign a variable to a sequence/user prompt?
 $ComponentName = Read-Host -prompt "Enter React Component Name"
+$ComponentType = Read-Host -prompt "what HTML element will it return?"
+
+$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8' # >> defaults to UTF-16 so we need to change this
 
 # the expression -z string is TRUE IF the lenght of string is zero
-if ( $ComponentName.length -eq 0 ) 
-{
-    Write-Output "You must specify a component name. Pascal Case is recommended for React components."
+if ($ComponentName.length -eq 0) {
+Write-Output "You must specify a component name. Pascal Case is recommended for React components."
 } 
-else
+else 
 {
-    # Create component directory.
+# Create component directory.
     New-Item -Path ./$ComponentName -itemType directory;
 
-    # Create component files.
+    # Create component file:
     New-Item -Path ./$ComponentName/$ComponentName.js;
-    New-Item -Path ./$ComponentName/$ComponentName.css;
 
-    # Write component CSS file.
-    Write-Output "  /* Add Component CSS here. */" >> ./$ComponentName/$ComponentName.css;
-   
+
     # in PowerShell if you use double-quotes you can reference the variable directly in the string!
     # Write component JS file.
-    Write-Output "import './$ComponentName.css';" >> ./$ComponentName/$ComponentName.js
+    Write-Output "import styled from 'styled-components';" >> ./$ComponentName/$ComponentName.js
     Write-Output "" >> ./$ComponentName/$ComponentName.js
     Write-Output "export default function $ComponentName(props) {" >> ./$ComponentName/$ComponentName.js
     Write-Output "    return (" >> ./$ComponentName/$ComponentName.js
-    Write-Output "        <div>" >> ./$ComponentName/$ComponentName.js
-    Write-Output '            <div></div>' >> ./$ComponentName/$ComponentName.js
-    Write-Output "        </div>" >> ./$ComponentName/$ComponentName.js
+    Write-Output "        <Styled$ComponentName>>" >> ./$ComponentName/$ComponentName.js
+    Write-Output "        </Styled$ComponentName>>" >> ./$ComponentName/$ComponentName.js
     Write-Output "    );" >> ./$ComponentName/$ComponentName.js
     Write-Output "}" >> ./$ComponentName/$ComponentName.js
+    Write-Output "const Styled$ComponentName = styled.$ComponentType;" >> ./$ComponentName/$ComponentName.js
 }
 
 exit
